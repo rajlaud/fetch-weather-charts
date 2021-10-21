a4b33cce706b472fe2763ed0bf7608be7ca9a951
Raj Laud <raj.laud@gmail.com>
2021-10-21 11:55:42 -0400

    Automatically sequence and rotate charts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import csv
 import os
 import requests
+from PIL import Image
 
 PREFIX = "https://tgftp.nws.noaa.gov/fax/"
 SUFFIX = ".TIF"
@@ -11,11 +12,18 @@ CHART_DIR = "charts/"
 
 with open(CHART_LIST, "r") as csvfile:
     reader = csv.DictReader(csvfile)
+    count = {}
+
     for row in reader:
+        if row["type"] not in count:
+            count[row["type"]] = 1
+        else:
+            count[row["type"]] += 1
+
         filename = row["filename"] + SUFFIX
         url = PREFIX + filename
         destination = CHART_DIR + row["type"] + "/"
-        destination_file = destination + filename
+        destination_file = f"{destination}{count[row['type']]} - {filename}"
         etag_file = destination + ".etag_" + filename
 
         os.makedirs(destination, exist_ok=True)
@@ -35,6 +43,10 @@ with open(CHART_LIST, "r") as csvfile:
                     download.write(file_request.content)
                 with open(etag_file, "w") as efile:
                     efile.write(r.headers["etag"])
+                if row["rotate"].isnumeric() and float(row["rotate"]) > 0.0:
+                    image = Image.open(destination_file)
+                    image = image.rotate(float(row["rotate"]), expand=True)
+                    image.save(destination_file)
             else:
                 print(f"Skipping chart {filename} because it is unmodified.")
         except requests.HTTPError as error:
