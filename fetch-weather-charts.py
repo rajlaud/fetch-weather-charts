1f51927ea70645b2b1609539f3ea196054fc3d50
Raj Laud <raj.laud@gmail.com>
2022-01-27 14:04:05 -0500

    Add support for joining overlapping images

--- fetch-weather-charts.py
+++ fetch-weather-charts.py
@@ -13,6 +13,7 @@ CHART_DIR = "charts/"
 with open(CHART_LIST, "r") as csvfile:
     reader = csv.DictReader(csvfile)
     count = {}
+    join_groups = {}
 
     for row in reader:
         if row["type"] not in count:
@@ -51,3 +52,36 @@ with open(CHART_LIST, "r") as csvfile:
                 print(f"Skipping chart {filename} because it is unmodified.")
         except requests.HTTPError as error:
             print(f"Download of {url} failed with {error}")
+        if row["join_group"] != "":
+            group = join_groups.setdefault(row["join_group"], [])
+            group.append(
+                {
+                    "image_file": destination_file,
+                    "destination": destination,
+                    "overlap": int(row["join_overlap"]),
+                }
+            )
+
+    for join_group, images_info in join_groups.items():
+        destination = images_info[0]["destination"]  # items should be in one folder
+
+        images_with_overlap = []
+        total_width = 0
+        max_height = 0
+        for item in images_info:
+            image = Image.open(item["image_file"])
+            total_width += image.size[0] - item["overlap"]
+            if max_height < image.size[1]:
+                max_height = image.size[1]
+            images_with_overlap.append((image, item["overlap"]))
+
+        new_image = Image.new("L", (total_width, max_height))
+
+        x_offset = 0
+        for image, overlap in images_with_overlap:
+            new_image.paste(
+                image.crop((overlap, 0, image.size[0], image.size[1])), (x_offset, 0)
+            )
+            x_offset += image.size[0]
+
+        new_image.save(f"{destination}/{join_group}.TIF")
